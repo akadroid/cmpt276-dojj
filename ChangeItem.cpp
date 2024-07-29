06f44e4231ce02dedb68522d9fa43033acb612b1
akadroid <danny@akadroid.net>
2024-07-29 00:30:46 -0700

    added clear to clear flags

--- ChangeItem.cpp
+++ ChangeItem.cpp
@@ -179,7 +179,10 @@ bool ChangeItemFile::seekToBeginningOfFile()
     cerr << "File is not open" << endl;
     return false;
   }
+
+  file.clear();
   file.seekg(0);
+
   return file.good(); // Returns true if the seek was successful
 }
 
